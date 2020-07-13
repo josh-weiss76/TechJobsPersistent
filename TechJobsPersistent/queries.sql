91395dd7b748b64100c5828003920079f30a0ccc
Joshua Weiss <sgtkorea9@yahoo.com>
2020-07-13 11:20:26 -0400

    Completed step 1

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,4 +1,8 @@
 --Part 1
+--employers (Id (int), Name (longtext), Location (longtext))
+--jobs (Id (int), Name (longtext), EmployerId (int))
+--jobskills (JobId (int), SkillId (int))
+--skills (Id (int), Name (longtext), Description (longtext))
 
 --Part 2
 
